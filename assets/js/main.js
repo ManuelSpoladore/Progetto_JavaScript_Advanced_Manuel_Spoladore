b172db9ff5d9ca3b93d36e8bda5caf789131570d
manuelspoladore <manuelspoladore@gmail.com>
2025-02-03 09:13:35 +0100

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,13 +145,13 @@ function createBooks(books) {
   setupCanvas();
 }
 
-// const fetchBooksDebounced = _.debounce(async () => {
-//   await getBooksBooks();
-// }, 500);
+const fetchBooksDebounced = _.debounce(async () => {
+  await getBooks();
+}, 500);
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-//   fetchBooksDebounced();
+  fetchBooksDebounced();
 });
 
 submitBtn.addEventListener("click", async (e) => {
